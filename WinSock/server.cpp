fde86eb9207d77f079f1cf0f7ad64f2e3ea2ee7d
kr-manish <kumar8manish@gmail.com>
2016-12-22 12:37:39 +0530

    First commit

---
+++ WinSock/server.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include <WinSock2.h>
+#include <Windows.h>
+#include <iostream>
+
+#define DEFAULT_PORT "27015"
+
+int server(void){
+
+	struct addrinfo *result = NULL,
+		*ptr = NULL,
+		hints;
+
+
+	return 0;
+}
